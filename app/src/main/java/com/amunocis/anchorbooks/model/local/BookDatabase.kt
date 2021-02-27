8acbe937236b75e7e877e696c82d2bed1e76b1cf
amunocis <amunocis@gmail.com>
2021-02-27 10:42:27 -0300

    feat: database file added

---
+++ app/src/main/java/com/amunocis/anchorbooks/model/local/BookDatabase.kt
@@ -0,0 +1,33 @@
+package com.amunocis.anchorbooks.model.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.amunocis.anchorbooks.model.Book
+
+
+@Database(entities = [Book::class], version = 1, exportSchema = false)
+abstract class BookDatabase: RoomDatabase() {
+    abstract fun bookDao(): BookDao
+
+    companion object {
+        private var INSTANCE: BookDatabase? = null
+
+        fun getDatabase(context: Context): BookDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    BookDatabase::class.java,
+                    "book_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
